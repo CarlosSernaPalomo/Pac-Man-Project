5ad8b7442f8972b9d20c45969a79804a37f4059a
Carlos-Serna-Palomo <Carlos.Serna4@alu.uclm.es>
2020-04-28 19:58:09 +0200

    Update Partida.cpp

--- Partida.cpp
+++ Partida.cpp
@@ -1,37 +1,126 @@
-#include <ctime>
+#include <chrono>
+
+using namespace std;
 
 class Partida{
+	
 	public:
 		
 		Cocos cocos;
-		Supercocos supercocos;
+		Cocos supercocos;
 		Comecocos comecocos;
 		FantasmaRojo fantasmaRojo;
 		FantasmaRosa fantasmaRosa;
-		FantasmaCian fantasmaCian;
 		FantasmaNaranja fantasmaNaranja;
+		FantasmaCian fantasmaCian;
 		
 		void Partida(Mapa mapa);
-		void ~Partida();
-		Mapa LeerMapa(); //Este metodo en principio creo que no va a hacer falta (como tampoco va a hacer falta incorporar el objeto mapa al objeto partida, ya que se usa al construir la partida y nunca mas, pero creo que no esta de mas que la partida contenga el mapa)
-		bool LeerEnCurso();
-		void ActualizarEnCurso();
-		bool LeerVictoria();
+		Mapa LeerMapa(); //Por si se desea consultar el mapa desde fuera del objeto, aunque en principio no es necesario
+		bool LeerVictoria(); //Puede que no sea necesario
 		void ActualizarVictoria();
-		bool LeerHuida();
-		void ActivarHuida();
-		void Desactivar Huida();
+		void ActualizarEnCurso();
+		void ActualizarHuida();
+		void Comer();
 		void Jugar();
 		
+		
 	private:
+		
 		Mapa mapa;
-		bool enCurso;
-		bool victoria;
-		bool huida;
-		time_t cronoHuida;
+		bool enCurso = true;
+		bool victoria = false;
+		bool huida = false;
+		auto tComienzoHuida = chrono::steady_clock::now();
+		auto tComienzoPartida = chrono::steady_clock::now();
+		
 };
 
 void Partida::Partida(Mapa mapa){
+		
+		//Almacenamiento del mapa	
 		this->mapa = mapa;
-		cocos 
+		
+		//Inicializacion del objeto cocos
+		cocos.InicializarMapaDeCocos(mapa.distribCocos);
+		cocos.InicializarCocosRestantes(mapa.numCocos);
+		
+		//Inicializacion del objeto supercocos
+		supercocos.InicializarMapaDeCocos(mapa.distribSupercocos);
+		supercocos.InicializarCocosRestantes(mapa.numSupercocos);
+		
+		//Inicializacion del objeto comecocos
+		comecocos.InicializarCasillasHabitables(mapa.paredes, mapa.coorPuerta);
+		comecocos.InicializarCoor(mapa.coorIniComecocos);
+		comecocos.InicializarDir(mapa.dirIniComecocos);
+		comecocos.InicializarVidas(mapa.vidasIni);
+		
+		//Inicializacion del objeto fantasmaRojo
+		fantasmaRojo.InicializarCasillasHabitables(mapa.paredes);
+		fantasmaRojo.InicializarCoor(mapa.coorIniFantasmaRojo);
+		
+		//Inicializacion del objeto fantasmaRosa
+		fantasmaRosa.InicializarCasillasHabitables(mapa.paredes);
+		fantasmaRosa.InicializarCoor(mapa.coorIniFantasmaRosa);
+		
+		//Inicializacion del objeto fantasmaNaranja
+		fantasmaNaranja.InicializarCasillasHabitables(mapa.paredes);
+		fantasmaNaranja.InicializarCoor(mapa.coorIniFantasmaNaranja);
+		
+		//Inicializacion del objeto fantasmaCian
+		fantasmaCian.InicializarCasillasHabitables(mapa.paredes);
+		fantasmaCian.InicializarCoor(mapa.coorIniFantasmaCian);
+		
 }
+
+Mapa Partida::LeerMapa(){
+	return mapa;
+}
+
+bool Partida::LeerVictoria(){
+	return victoria;
+}
+
+void Partida::ActualizarVictoria(){
+	if(cocos.LeerCocosRestantes() + supercocos.LeerCocosRestantes() == 0){
+		victoria = true;
+	}
+}
+
+bool Partida::LeerEnCurso(){
+	return enCurso;
+}
+
+void Partida::ActualizarEnCurso(){
+	if(chrono::steady_clock::now() - tComienzoPartida > LeerMapa().duracionPartida || comecocos.LeerVidas() == 0 || victoria){
+		enCurso = false;
+	}
+}
+
+void Partida::ActualizarHuida(){
+	if(chrono::steady_clock::now() - tComienzoHuida > LeerMapa().duracionHuida){
+		huida = false;
+	}
+}
+
+void Partida::Comer(){
+	
+}
+
+bool Partida::Jugar(){
+	
+	while(enCurso){
+		
+		
+		
+	}
+	
+	if(victoria){
+		lo que sea;
+	}else{
+		lo que sea;
+	}
+	
+}
+
+
+
