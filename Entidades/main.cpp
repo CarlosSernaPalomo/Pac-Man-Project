8fb4b5c90369aa4c293b8bf024e913e645934d1d
Carlos-Serna-Palomo <Carlos.Serna4@alu.uclm.es>
2020-05-01 14:32:24 +0200

    Branch rename

--- Entidades/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "cabeceras.h"
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) {
-	Comecocos a;
-	a.ActualizarDirDeseada();
-	return 0;
-}
