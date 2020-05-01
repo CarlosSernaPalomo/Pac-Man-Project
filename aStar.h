7ae7a1480a59958170600c2700a81613704b2d64
Ivan-Castillero <62428384+Ivan-Castillero@users.noreply.github.com>
2020-05-01 16:51:15 +0200

    Add files via upload

---
+++ aStar.h
@@ -0,0 +1,301 @@
+#include <list>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class point {
+public:
+    point( int a = 0, int b = 0 ) { x = a; y = b; } 
+    bool operator ==( const point& o ) { return o.x == x && o.y == y; }
+    point operator +( const point& o ) { return point( o.x + x, o.y + y ); }
+    int x, y;
+};
+
+//Mapa para los fantasmas
+class map {	
+public:
+    map() { 
+        char t[29][26] = {	//0=Pasillo, 1=Pared
+          //00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//00
+			{0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0},//01
+			{0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0},//02
+			{0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0},//03
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//04
+			
+			{0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0},//05
+			{0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0},//06
+			{0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},//07
+			{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},//08
+			{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},//09
+			
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1},//10	
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1},//11
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1},//12
+			{1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1},//13
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1},//14
+			
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1},//15			
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1},//16
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1},//17
+			{1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1},//18
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//19
+			
+			{0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0},//20	
+			{0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0},//21
+			{0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0},//22
+			{1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1},//23
+			{1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1},//24
+			
+			{0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},//25		
+			{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//26
+			{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//27
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//28
+        };
+        w = 26;
+		h = 29;
+        for( int r = 0; r < h; r++ )
+            for( int s = 0; s < w; s++ )
+                m[s][r] = t[r][s];
+    }
+    int operator() ( int x, int y ) { return m[x][y]; }
+    char m[26][29];
+    int w, h;
+	void ponepared (int,int);
+	void ponepasillo (int,int);
+};
+
+//Mapa para el jugador
+class mapajugador {	
+public:
+    mapajugador() { 
+        char t[29][26] = {	//0=Pasillo, 1=Pared, 2=Coco, 3=Supercoco
+          //00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},//00
+			{2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2},//01
+			{3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3},//02
+			{2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2},//03
+			{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},//04
+			
+			{2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2},//05
+			{2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2},//06
+			{2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2},//07
+			{1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},//08
+			{1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},//09
+			
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1},//10	
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1},//11
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1},//12
+			{1, 1, 1, 1, 1, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 1, 1, 1, 1, 1},//13
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1},//14
+			
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1},//15			
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1},//16
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1},//17
+			{1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1},//18
+			{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},//19
+			
+			{2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2},//20	
+			{3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3},//21
+			{2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2},//22
+			{1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1},//23
+			{1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1},//24
+			
+			{2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2},//25		
+			{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},//26
+			{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},//27
+			{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},//28
+        };
+        w = 26;
+		h = 29;
+        for( int r = 0; r < h; r++ )
+            for( int s = 0; s < w; s++ )
+                m[s][r] = t[r][s];
+    }
+    int operator() ( int x, int y ) { return m[x][y]; }
+    char m[26][29];
+    int w, h;
+	void ponepasillo (int,int);
+};
+
+
+class node {
+public:
+    bool operator == (const node& o ) { return pos == o.pos; }
+    bool operator == (const point& o ) { return pos == o; }
+    bool operator < (const node& o ) { return dist + cost < o.dist + o.cost; }
+    point pos, parent;
+    int dist, cost;
+};
+
+//Algoritmo A*
+class aStar {
+public:
+    aStar() {
+        neighbours[0] = point( -1, -1 ); neighbours[1] = point(  1, -1 );
+        neighbours[2] = point( -1,  1 ); neighbours[3] = point(  1,  1 );
+        neighbours[4] = point(  0, -1 ); neighbours[5] = point( -1,  0 );
+        neighbours[6] = point(  0,  1 ); neighbours[7] = point(  1,  0 );
+    }
+ 
+    int calcDist( point& p ){
+        // need a better heuristic
+        int x = end.x - p.x, y = end.y - p.y;
+        return( x * x + y * y );
+    }
+ 
+    bool isValid( point& p ) {
+        return ( p.x >-1 && p.y > -1 && p.x < m.w && p.y < m.h );
+    }
+ 
+    bool existPoint( point& p, int cost ) {
+        list<node>::iterator i;
+        i = find( closed.begin(), closed.end(), p );
+        if( i != closed.end() ) {
+            if( ( *i ).cost + ( *i ).dist < cost ) return true;
+            else { closed.erase( i ); return false; }
+        }
+        i = std::find( open.begin(), open.end(), p );
+        if( i != open.end() ) {
+            if( ( *i ).cost + ( *i ).dist < cost ) return true;
+            else { open.erase( i ); return false; }
+        }
+        return false;
+    }
+ 
+    bool fillOpen( node& n ) {
+        int stepCost, nc, dist;
+        point neighbour;
+ 
+        for( int x = 0; x < 8; x++ ) {
+            // one can make diagonals have different cost
+            stepCost = x < 4 ? 1 : 1;
+            neighbour = n.pos + neighbours[x];
+            if( neighbour == end ) return true;
+ 
+            if( isValid( neighbour ) && m( neighbour.x, neighbour.y ) != 1 ) {
+                nc = stepCost + n.cost;
+                dist = calcDist( neighbour );
+                if( !existPoint( neighbour, nc + dist ) ) {
+                    node m;
+                    m.cost = nc; m.dist = dist;
+                    m.pos = neighbour; 
+                    m.parent = n.pos;
+                    open.push_back( m );
+                }
+            }
+        }
+        return false;
+    }
+ 
+    bool search( point& s, point& e, map& mp ) {
+        node n; end = e; start = s; m = mp;
+        n.cost = 0; n.pos = s; n.parent = 0; n.dist = calcDist( s ); 
+        open.push_back( n );
+        while( !open.empty() ) {
+            //open.sort();
+            node n = open.front();
+            open.pop_front();
+            closed.push_back( n );
+            if( fillOpen( n ) ) return true;
+        }
+        return false;
+    }
+ 
+    int path( list<point>& path ) {
+        path.push_front( end );
+        int cost = 1 + closed.back().cost; 
+        path.push_front( closed.back().pos );
+        point parent = closed.back().parent;
+ 
+        for( list<node>::reverse_iterator i = closed.rbegin(); i != closed.rend(); i++ ) {
+            if( ( *i ).pos == parent && !( ( *i ).pos == start ) ) {
+                path.push_front( ( *i ).pos );
+                parent = ( *i ).parent;
+            }
+        }
+        path.push_front( start );
+        return cost;
+    }
+    map m; point end, start;
+    point neighbours[8];
+    list<node> open;
+    list<node> closed;
+};
+
+void map::ponepared (int x, int y){	//Funcion para cambiar coordenada a pared del mapa fantasmas
+	m[x][y]=1;
+}
+
+void map::ponepasillo (int x, int y){	//Funcion para cambiar coordenada a pasillo del mapa fantasmas
+	m[x][y]=0;
+}
+
+void mapajugador::ponepasillo (int x, int y){	//Funcion para cambiar coordenada a pasillo del mapa jugador
+	m[x][y]=0;
+}
+
+//Funcion para buscar la siguente posicion de los fantasmas
+point nextpos (point s,point e,map m){	//s=punto partida, e=punto destino, m=mapa fantasmas
+	aStar as;
+	point pos0,pos1,pos2;
+	int k=0;
+	if( as.search( s, e, m ) ) {
+        list<point> path;
+        int c = as.path( path );
+        /*for( int y = -1; y < 30; y++ ) {  //Bucle para mostrar por pantalla el camino del fantasma si fuera necesario
+            for( int x = -1; x < 27; x++ ) {
+                if( x < 0 || y < 0 || x > 25 || y > 28 || m( x, y ) == 1 )
+                    cout << char(0xdb);
+                else {
+                    if( find( path.begin(), path.end(), point( x, y ) )!= path.end() )
+                        cout << "X";
+                    else cout << " ";
+                }
+            }
+            cout << "\n";
+        }*/
+        for( list<point>::iterator i = path.begin(); i != path.end(); i++ ) {
+        	if (k==0){
+        		pos0.x=( *i ).x;
+        		pos0.y=( *i ).y;
+				k++;	
+			}else if (k==1){
+				pos1.x=( *i ).x;
+        		pos1.y=( *i ).y;
+				k++;
+			}else if (k==2){
+				pos2.x=( *i ).x;
+        		pos2.y=( *i ).y;
+				k++;
+			}            
+        }
+    }    
+    if (pos1==pos0)
+    	return pos2;
+    else return pos1;
+};	
+
+//Funcion para mostrar por pantalla mapa jugador
+void visualizamapa (mapajugador m){
+	for( int y = -1; y < 30; y++ ) {
+        for( int x = -1; x < 27; x++ ) {
+            if( x < 0 || y < 0 || x > 25 || y > 28 || m( x, y ) == 1 ) //Pared
+                cout << char(0xdb); 
+            else{
+				if(m( x, y ) == 0) //Pasillo
+                	cout << " ";
+            	else if(m( x, y ) == 2)	//Coco
+                	cout << ".";
+				else if(m( x, y ) == 3) //Supercoco
+					cout << "X";
+        	}
+    	}
+		cout << "\n";
+	}
+}
+
+
+
+
+
