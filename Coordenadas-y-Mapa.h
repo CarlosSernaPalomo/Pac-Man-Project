e67b1f505b3c7e63d6daa398b3f9f01f0f1317b4
Carlos-Serna-Palomo <Carlos.Serna4@alu.uclm.es>
2020-05-02 13:13:39 +0200

    Update Coordenadas-y-Mapa.h

--- Coordenadas-y-Mapa.h
+++ Coordenadas-y-Mapa.h
@@ -5,57 +5,57 @@ using namespace std;
 
 class Coordenadas {
 public:
-    Coordenadas( int a = 0, int b = 0 ) { x = a; y = b; } 
-    bool operator ==( const Coordenadas& o ) { return o.x == x && o.y == y; }
-    Coordenadas operator +( const Coordenadas& o ) { return Coordenadas( o.x + x, o.y + y ); }
-    int x, y;
+	Coordenadas( int a = 0, int b = 0 ) { x = a; y = b; } 
+	bool operator ==( const Coordenadas& o ) { return o.x == x && o.y == y; }
+	Coordenadas operator +( const Coordenadas& o ) { return Coordenadas( o.x + x, o.y + y ); }
+	int x, y;
 };
 
 
 class Mapa {	
 public:
-		
+	
 	//Constructor por defecto (crea un mapa de 0s)
 	Mapa(){
-	w = 28;
-	h = 31;
-	for( int r = 0; r < h; r++ )
-	for( int s = 0; s < w; s++ )
-	m[s][r] = '0';
-	}
-
-    //Constructor a partir de semilla (crea un mapa a partir de un array bidimensional)
-    Mapa(char semilla[31][28]) { 
-        w = 28;
+		w = 28;
 		h = 31;
-        for( int r = 0; r < h; r++ )
-            for( int s = 0; s < w; s++ )
-                m[s][r] = semilla[r][s];
+		for( int r = 0; r < h; r++ )
+			for( int s = 0; s < w; s++ )
+				m[s][r] = '0';
+	}
+	
+	//Constructor a partir de semilla (crea un mapa a partir de un array bidimensional)
+	Mapa(char semilla[31][28]) { 
+		w = 28;
+		h = 31;
+		for( int r = 0; r < h; r++ )
+			for( int s = 0; s < w; s++ )
+				m[s][r] = semilla[r][s];
     }
-    
-    //Variables
-    char m[28][31];
-    int w, h;
-    
-    //Metodos
-    int operator() (Coordenadas c) { return m[c.x][c.y]; }
+	
+	//Variables
+	char m[28][31];
+	int w, h;
+	
+	//Metodos
+	int operator() (Coordenadas c) { return m[c.x][c.y]; }
 	void Modificar(Coordenadas c, char valor) { m[c.x][c.y] = valor; }
 	void Imprimir(){
-		Coordenadas posicionActual;
+	Coordenadas posicionActual;
 		for( int y = 0; y < h; y++ ) {
-	        for( int x = 0; x < w; x++ ) {
+			for( int x = 0; x < w; x++ ) {
 				if(m[x][y] == 0) //Pasillo
-                	cout << " ";
-                else if(m[x][y] == 1) //Pared
-            		cout << char(0xdb);
-            	else if(m[x][y] == 2)	//Coco
-                	cout << char(250);
+					cout << " ";
+				else if(m[x][y] == 1) //Pared
+					cout << char(0xdb);
+				else if(m[x][y] == 2)	//Coco
+					cout << char(250);
 				else if(m[x][y] == 3) //Supercoco
 					cout << "O";
-	        	else if(m[x][y] == 4) //Puerta del refugio
-	        		cout << char(176);
-	        	}
-	        cout << endl;
-	    	}
+				else if(m[x][y] == 4) //Puerta del refugio
+					cout << char(176);
+			}
+			cout << endl;
+		}
 	}
 };
