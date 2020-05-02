fb01dde8fb86634b4491fb5c5800e3b157e1bffc
Carlos-Serna-Palomo <Carlos.Serna4@alu.uclm.es>
2020-05-02 14:05:22 +0200

    Update Cocos.h

--- Cocos.h
+++ Cocos.h
@@ -1,6 +1,6 @@
 class Cocos{
 	public:
-		void InicializarCocos(Nivel);
+		void InicializarCocos(Mapa, int);
 		Mapa LeerMapaDeCocos(Coordenadas);
 		int LeerCocosRestantes();
 		void EliminarCoco(Coordenadas);
@@ -9,6 +9,14 @@ class Cocos{
 		int cocosRestantes;
 }
 
-void Cocos::InicializarCocos(Nivel nivel){
+void Cocos::InicializarCocos(Mapa mapa, int n){
+	
+	for(int r = 0; r < mapa.h; r++){
+		for(int s = 0; s < m.w; s++){
+			if(mapa.m[s][r] == 2) mapaDeCocos.m[s][r] = 1;
+		}
+	}
+	
+	cocosRestantes = n;
 	
 }
