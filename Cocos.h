1a61f9ba1df0d3adb9be4658b4f244b3d1c04ff1
Carlos-Serna-Palomo <Carlos.Serna4@alu.uclm.es>
2020-05-02 13:06:11 +0200

    Update Cocos.h

--- Cocos.h
+++ Cocos.h
@@ -1,1 +1,14 @@
+class Cocos{
+	public:
+		void InicializarCocos(Nivel);
+		Mapa LeerMapaDeCocos(Coordenadas);
+		int LeerCocosRestantes();
+		void EliminarCoco(Coordenadas);
+	private:
+		Mapa mapaDeCocos;
+		int cocosRestantes;
+}
 
+void Cocos::InicializarCocos(Nivel nivel){
+	
+}
